9cbcbd6701b2ef2a42eea22f3059a2d47934f065
Francisco García <fco.garcia.delgado@gmail.com>
2023-02-06 19:30:21 +0100

    ControllerAdvice-ExceptionController

--- src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
@@ -21,6 +21,7 @@ public class DashboardController {
     public String displayDashboard(Model model,Authentication authentication) {
         model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+        //throw new RuntimeException("It´s been a bad day!");
         return "dashboard.html";
     }
 
