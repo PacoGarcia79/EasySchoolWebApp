8260a9f31114ae13a3f387dc4c927afe956440c7
Francisco García <fco.garcia.delgado@gmail.com>
2023-03-02 22:28:23 +0100

    Holidays in H2 DB

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,13 @@
+CREATE TABLE IF NOT EXISTS `holidays` (
+  `day` varchar(20) NOT NULL,
+  `reason` varchar(100) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
 CREATE TABLE IF NOT EXISTS `contact_msg` (
   `contact_id` int AUTO_INCREMENT  PRIMARY KEY,
   `name` varchar(100) NOT NULL,
@@ -11,3 +21,4 @@ CREATE TABLE IF NOT EXISTS `contact_msg` (
   `updated_at` TIMESTAMP DEFAULT NULL,
   `updated_by` varchar(50) DEFAULT NULL
 );
+
