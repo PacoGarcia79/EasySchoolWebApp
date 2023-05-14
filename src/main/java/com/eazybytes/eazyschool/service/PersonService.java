9f2736a264cc68ffe894501ca9eb22fd1f24df81
Francisco García <fco.garcia.delgado@gmail.com>
2023-05-14 20:16:16 +0200

    Custom auth and passw hash first commit

--- src/main/java/com/eazybytes/eazyschool/service/PersonService.java
+++ src/main/java/com/eazybytes/eazyschool/service/PersonService.java
@@ -1,15 +1,14 @@
 package com.eazybytes.eazyschool.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import com.eazybytes.eazyschool.constants.EazySchoolConstants;
-import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.model.Person;
 import com.eazybytes.eazyschool.model.Roles;
-import com.eazybytes.eazyschool.repository.ContactRepository;
 import com.eazybytes.eazyschool.repository.PersonRepository;
 import com.eazybytes.eazyschool.repository.RolesRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
@@ -20,10 +19,14 @@ public class PersonService {
     @Autowired
     private RolesRepository rolesRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public boolean createNewPerson(Person person){
         boolean isSaved = false;
         Roles role = rolesRepository.getByRoleName(EazySchoolConstants.STUDENT_ROLE);
         person.setRoles(role);
+        person.setPwd(passwordEncoder.encode(person.getPwd()));
         person = personRepository.save(person);
         if (null != person && person.getPersonId() > 0)
         {
